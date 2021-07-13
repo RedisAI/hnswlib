bdc53dd8e40e2746a5e0abcce48fd4a7ef2eddc8
alonre24 <alonreshef24@gmail.com>
2021-07-13 23:40:45 +0300

    update benchmark script for d is 128

--- examples/run_tests.sh
+++ examples/run_tests.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-for d in 4 32 128 512
+for d in 128 512
 do
-	for n in 1000 10000 100000 1000000
+	for n in 10000 100000 1000000
 	do
-		for M in 6 12 24 48 64
+		for M in 24 48 64
 		do
-			for ef in 10 100 200 500
+			for ef in 200 500 1000
 			do
 				k=${M}
-				if [[ ${M} -gt ${ef} ]]; then
-				 	k=${ef}
-				fi
 				res=$(./test_hnsw d ${d} n ${n} k ${k} M ${M} ef_c ${ef} ef ${ef})
 				if (( $(echo "$res > 0.9" |bc -l) )); then
 					break
