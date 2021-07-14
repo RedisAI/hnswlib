c3792f82863bab0142e4b42305c25092df806746
alonre24 <alonreshef24@gmail.com>
2021-07-14 09:26:59 +0300

    update benchmark script for d is 128 (M=24 ef=500)

--- examples/run_tests.sh
+++ examples/run_tests.sh
@@ -2,11 +2,11 @@
 
 for d in 128 512
 do
-	for n in 10000 100000 1000000
+	for n in 1000000
 	do
 		for M in 24 48 64
 		do
-			for ef in 200 500 1000
+			for ef in 500 1000
 			do
 				k=${M}
 				res=$(./test_hnsw d ${d} n ${n} k ${k} M ${M} ef_c ${ef} ef ${ef})
