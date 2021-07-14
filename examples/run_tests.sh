a92424cd0a86abedff2ede526a8892ca9c10e0cd
alonre24 <alonreshef24@gmail.com>
2021-07-15 01:23:28 +0300

    update benchmark

--- examples/run_tests.sh
+++ examples/run_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for d in 128 512
+for d in 32 128 512
 do
 	for n in 1000000
 	do
-		for M in 24 48 64
+		for M in 12 24 48 64
 		do
-			for ef in 500 1000
+			for ef in 100 200 500
 			do
-				k=${M}
+				k=10
 				res=$(./test_hnsw d ${d} n ${n} k ${k} M ${M} ef_c ${ef} ef ${ef})
 				if (( $(echo "$res > 0.9" |bc -l) )); then
 					break
