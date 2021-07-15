696b63a7f47f980581a1c4035f21a632c8b12b7b
alonre24 <alonreshef24@gmail.com>
2021-07-15 13:13:47 +0300

    update benchmark

--- examples/run_tests.sh
+++ examples/run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for d in 32 128 512
+for d in 128 512
 do
 	for n in 1000000
 	do
-		for M in 12 24 48 64
+		for M in 24 48 64
 		do
-			for ef in 100 200 500
+			for ef in 200 500
 			do
 				k=10
 				res=$(./test_hnsw d ${d} n ${n} k ${k} M ${M} ef_c ${ef} ef ${ef})
